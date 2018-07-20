3c2de515a2669069dc03436c3484476aff7df797
Eric Wellnitz <me@ericwellnitz.com>
2018-07-20 08:59:35 -0400

    Added  CustomActivites solution

---
+++ CustomActivities/CustomActivities/CodeConvertToJson.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Activities;
+using System.Drawing;
+
+namespace CustomActivities
+{
+    [ToolboxBitmap(typeof(CodeConvertToJson), "ConvertToJson.png")]
+    public class CodeConvertToJson : CodeActivity<string>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CodeConvertToJson" /> class.
+        /// </summary>
+        public CodeConvertToJson()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the text.
+        /// </summary>
+        /// <value>
+        /// The text.
+        /// </value>
+        [RequiredArgument]
+        public InArgument<object> Object { get; set; }
+
+        /// <summary>
+        /// When implemented in a derived class, performs the execution of the activity.
+        /// </summary>
+        /// <param name="context">The execution context under which the activity executes.</param>
+        /// <returns>
+        /// The result of the activity’s execution.
+        /// </returns>
+        protected override string Execute(CodeActivityContext context)
+        {
+            var instance = Object.Get(context);
+            return instance == null ? "null" : JsonConvert.SerializeObject(instance, Formatting.Indented,
+                new JsonSerializerSettings() { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+        }
+    }
+}
---
+++ CustomActivities/CustomActivities/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using Cmc.Core.Reflection;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CustomActivities")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomActivities")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ExtensionAssembly]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e3e3b2c5-6650-425a-9dea-9302e7d16f0c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
